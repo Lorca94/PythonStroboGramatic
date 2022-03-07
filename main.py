da9be832ccc222566c59e6b43b701f37ec5eca0e
lorca94 <lorcanavarrov@gmail.com>
2022-03-07 22:45:02 +0100

    Cambio de nombre en las variables

--- main.py
+++ main.py
@@ -16,29 +16,28 @@
 # rango siempre deberá ser válido) """
 
 
-class Exercise:
-    def findAllStrobogramaticNumbers(firstNumber: int, lastNumber: int):
-        if firstNumber >= lastNumber:
-            return "Error: The first number entered must always have a value less than the second number entered"
-        else:
-            listStrobo = []
-            b = 0
-            for x in range(firstNumber, lastNumber + 1):
-                if x > 10:
-                    listProve = []
-                    xDigit = [int(a) for a in str(x)]
-                    for i in xDigit:
-                        if i == 6 or i == 9:
-                            if i == 6:
-                                listProve.append(9)
-                            else:
-                                listProve.append(6)
+def findallstrobogramaticnumbers(firstNumber: int, lastNumber: int):
+    if firstNumber >= lastNumber:
+        return "Error: The first number entered must always have a value less than the second number entered"
+    else:
+        listStrobo = []
+        b = 0
+        for x in range(firstNumber, lastNumber + 1):
+            if x > 10:
+                listProve = []
+                xDigit = [int(a) for a in str(x)]
+                for i in xDigit:
+                    if i == 6 or i == 9:
+                        if i == 6:
+                            listProve.append(9)
                         else:
-                            listProve.append(i);
-                    listProve.reverse()
-                    if listProve == xDigit:
-                        listStrobo.append(x)
-            return listStrobo
+                            listProve.append(6)
+                    else:
+                        listProve.append(i);
+                listProve.reverse()
+                if listProve == xDigit:
+                    listStrobo.append(x)
+        return listStrobo
 
 
-print(Exercise.findAllStrobogramaticNumbers(1, 2000));
+print(findallstrobogramaticnumbers(1, 2000));
